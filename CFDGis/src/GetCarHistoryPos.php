415234e74cb9c25a8f1a9bf924dbd70d4e23c7ad
huhongjun <huhongjun@gmail.com>
2009-01-08 12:11:09 +0000

    TanHW 20081225 初始版

---
+++ CFDGis/src/GetCarHistoryPos.php
@@ -0,0 +1,83 @@
+<?php
+
+if(!($conn=ora_logon("system@hlgis", "system"))) 
+{ 
+	echo "Error: Cannot connect to database\n"; 
+	exit; 
+} 
+
+$car_id = $_REQUEST["car_id"]; // 获取传入的车辆的ID
+$from_time = $_REQUEST["from_time"];    // 获取传入的开始时间
+$to_time = $_REQUEST["to_time"]; // 获取传入的终止时间
+$total_seconds = $_REQUEST["total_seconds"]; // 动画显示的总时间
+
+// 先得到记录数
+$sql = "SELECT count(*) vcount from SO_CAR_HISTORY_POS where car_id = ".$car_id." and cartime >= to_date('".$from_time."', 'yyyy-mm-dd hh24-mi-ss') and cartime <= to_date('".$to_time."', 'yyyy-mm-dd hh24-mi-ss') order by cartime";	//有查询条件
+$cursor = ora_open($conn);
+ora_parse($cursor, $sql, 0);
+ora_exec($cursor);
+$rec_count;
+while(ora_fetch($cursor)){
+	$rec_count= ora_getcolumn($cursor, 0);
+}
+
+// 开始查询
+$sql = "SELECT x, y, to_char(cartime,'yyyy-mm-dd hh24:mi:ss') cartime from SO_CAR_HISTORY_POS where car_id = ".$car_id." and cartime >= to_date('".$from_time."', 'yyyy-mm-dd hh24-mi-ss') and cartime <= to_date('".$to_time."', 'yyyy-mm-dd hh24-mi-ss') order by cartime";	//有查询条件
+
+$cursor = ora_open($conn);
+
+ora_parse($cursor, $sql, 0);
+ora_exec($cursor);
+
+// 返回结果的xml文档
+$car_hist_pos = "";
+$one_car_pos = "";
+
+// 按时间平分，每秒要显示的坐标记录数
+$seg_count = 1; // 每秒的记录数
+if($rec_count > 0)
+	$seg_count = ceil($rec_count / $total_seconds); // 截取整数部分
+
+//if($seg_count < 1) $seg_count = 1;
+
+$one_time_str;
+$time_str = "";
+
+$mod_index = 0;
+
+$index = 0;
+while(ora_fetch($cursor))
+{
+	//$car_id = ora_getcolumn($cursor, 0);
+	//$car_code = mb_convert_encoding(ora_getcolumn($cursor, 1), "UTF-8", "GB2312");
+	//$car_name = mb_convert_encoding(ora_getcolumn($cursor, 2), "UTF-8", "GB2312");;
+	
+	$index = $index + 1;
+	$mod_index = $index % $seg_count;
+	
+	
+	
+	if($mod_index == 1) 
+	{
+		$one_time_str = ora_getcolumn($cursor, 2);
+		if($index == 1)	$time_str = $one_time_str;
+		else $time_str = $time_str."/".$one_time_str;
+	}
+
+
+	$pos_x = ora_getcolumn($cursor, 0);
+	$pos_y = ora_getcolumn($cursor, 1);
+
+	$one_car_pos = $pos_x.":".$pos_y;
+	$car_hist_pos = $car_hist_pos.";".$one_car_pos;
+}
+
+$hist_pos_info = $time_str."&".$car_hist_pos;
+
+echo $hist_pos_info;
+
+ora_logoff($conn);
+
+
+
+?>
---
+++ CFDGis/src/Get_Car_TreeView.php
@@ -0,0 +1,103 @@
+<?php
+if ( stristr($_SERVER["HTTP_ACCEPT"],"application/xhtml+xml") ) 
+{ 
+	header("Content-type: application/xhtml+xml"); 
+} 
+else 
+{ 
+	header("Content-type: text/xml"); 
+} 
+
+echo("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"); 
+
+/* 	数据库连接参数	*/
+$dbms='mysql';    	//数据库类型 Oracle 用ODI
+$host='localhost';		//数据库主机名
+$dbName='gis';   	//使用的数据库
+$user='root';     		//数据库连接用户名
+$pass='root';         	//对应的密码
+$dsn="$dbms:host=$host;dbname=$dbName";
+
+
+$svgxml ="";
+
+
+	/*	从传入参数获得查询条件	*/
+	$group =$_REQUEST["id"];
+	if(!($conn=ora_logon("system@hlgis","system"))) 
+	{ 
+		echo "Error: Cannot connect to database\n"; 
+		exit; 
+	} 	
+	
+	
+	$sql = "SELECT ID,CAR_NAME from so_car_info where CAR_TYPE_ID="+group;	//有查询条件
+	//$sql = $sql.$where;
+
+	//$sqlall = "SELECT ID,X,Y,CAR_CODE,CAR_NAME,CAR_TYPE,CAR_OWNER from SO_CAR_CURRENT_POS ";				//无查询条件
+    $type1=mb_convert_encoding("柳工",   "UTF-8","GB2312");
+    $type2=mb_convert_encoding("龙工",   "UTF-8","GB2312");
+    $type3=mb_convert_encoding("联众",   "UTF-8","GB2312");
+	if ($group == 0){
+			$sql = "SELECT TYPE_ID,TYPE_NAME from SO_CAR_TYPE ";	//有查询条件
+			$cursor=ora_open($conn);
+			ora_parse($cursor,$sql,0); 
+			ora_exec($cursor);
+			$svgxml = '<tree id="0">';
+			while(ora_fetch($cursor)) 
+			{ 
+
+				$TYPE_ID = ora_getcolumn ($cursor, 0);
+				$TYPE_NAME = mb_convert_encoding(ora_getcolumn ($cursor, 1),   "UTF-8","GB2312");
+                
+	
+		        $typexml = '<item child="1" id="'.$TYPE_ID.'" text="'.$TYPE_NAME.'">
+							<userdata name="ud_block">ud_data</userdata>
+							</item>';
+				$svgxml = $svgxml .$typexml;
+						
+			} 
+			$svgxml = $svgxml."</tree>";	
+					
+			//输出给浏览器端的内容
+			echo $svgxml; 
+			
+	}
+	else{
+	    	$sql = "SELECT ID,X,Y,CAR_CODE,CAR_NAME,CAR_TYPE,CAR_OWNER from sde.SO_CAR_CURRENT_POS ";
+		$sql = "SELECT ID,CAR_NAME from SO_CAR_INFO where CAR_TYPE_ID= ".$group;
+			$cursor=ora_open($conn);
+			ora_parse($cursor,$sql,0); 
+			ora_exec($cursor);
+			while(ora_fetch($cursor)) 
+			{ 
+				$circleoid = ora_getcolumn ($cursor, 0);
+				$CAR_NAME = mb_convert_encoding(ora_getcolumn ($cursor, 1),   "UTF-8","GB2312");
+
+						$carinforxml = "<item child='0' id='".$circleoid."' text='".$CAR_NAME."'><userdata name='ud_block'>ud_data</userdata></item>";
+						
+						//拼装全部circle的SVG XML定义
+						$svgxml = $svgxml . $carinforxml;	
+						
+			} 
+			$svgxml  = "<tree id='" .$group."'>" . $svgxml . "</tree>";	
+					
+			//输出给浏览器端的内容
+			echo $svgxml; 
+			
+	}
+/*	help
+	采用PHP PDO开发，支持数据库无关性，
+	连接不同数据库只需修改dbms,可能的值有mysql/odi/mssql/postsql/odbc等
+	默认这个不是长连接，如果需要数据库长连接，需要最后加一个参数：array(PDO::ATTR_PERSISTENT => true) 变成这样：
+	$db=new PDO($dsn,$user,$pass,array(PDO::ATTR_PERSISTENT=>true));
+	测试：
+		http://localhost/chd/DGGeo_PDO.php
+		http://loclahost/chd/DBGeo_PDO.php?oid=1
+		故意设置错误的数据连接参数或sql语句，测试数据库错误消息处理
+	SVG文件中如何使用：
+		SVG中获得的文本来自echo的输出
+		绝对路径	http://localhost/chd/DGGeo_PDO.php
+		相对路径	DGGeo_PDO.php(SVG文件之在同一个目录)
+*/
+?>
